b74b3c341e94a124fadf4af5ecfb28a268e6b633
Enelith <jonathan.vinh@gmail.com>
2021-06-29 00:13:02 +0200

    Maj Part 45 - Bean Scopes - Write Some Code - Part 2

--- S06 - Spring Bean Scopes and Lifecycle/src/main/java/bean/scopes/lifecycle/BeanScopeDemoApplication.java
+++ S06 - Spring Bean Scopes and Lifecycle/src/main/java/bean/scopes/lifecycle/BeanScopeDemoApplication.java
@@ -21,10 +21,27 @@ public class BeanScopeDemoApplication {
 		    new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
 
 	// retrieve bean from spring container
-	ICoach alphaCoach = context.getBean("myCoach", ICoach.class);
+	ICoach alphaCoach = context.getBean("mySingletonCoach", ICoach.class);
+	ICoach betaCoach = context.getBean("mySingletonCoach", ICoach.class);
 
-	ICoach betaCoach = context.getBean("myCoach", ICoach.class);
+	// check if they're the same (Singleton by default)
+	boolean isEqual = (alphaCoach == betaCoach);
+	System.out.println("====== SINGLETON SCOPE ======");
+	System.out.println("\nPointing to the same object ? " + isEqual);
+	System.out.println("Memory location for alphaCoach : " + alphaCoach);
+	System.out.println("Memory location for betaCoach : " + betaCoach);
 
+	// retrieve bean from spring container
+	ICoach gammaCoach = context.getBean("myPrototypeCoach", ICoach.class);
+	ICoach zetaCoach = context.getBean("myPrototypeCoach", ICoach.class);
+
+	// check if they're the same (Defined as Prototype)
+	isEqual = (gammaCoach == zetaCoach);
+	System.out.println("\n====== PROTOTYPE SCOPE ======");
+	System.out.println("\nPointing to the same object ? " + isEqual);
+	System.out.println("Memory location for gammaCoach : " + gammaCoach);
+	System.out.println("Memory location for zetaCoach : " + zetaCoach);
+	
 	// close the context
 	context.close();
     }
