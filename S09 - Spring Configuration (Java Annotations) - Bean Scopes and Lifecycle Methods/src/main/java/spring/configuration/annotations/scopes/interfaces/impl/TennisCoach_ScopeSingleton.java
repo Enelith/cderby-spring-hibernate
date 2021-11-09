e5a37f62585271c7b73fe63cabd51fc9584aeab1
Enelith <jonathan.vinh@gmail.com>
2021-11-09 15:59:38 +0100

    Maj Part 81 - Bean Lifecycle Method Annotations

--- S09 - Spring Configuration (Java Annotations) - Bean Scopes and Lifecycle Methods/src/main/java/spring/configuration/annotations/scopes/interfaces/impl/TennisCoach_ScopeSingleton.java
+++ S09 - Spring Configuration (Java Annotations) - Bean Scopes and Lifecycle Methods/src/main/java/spring/configuration/annotations/scopes/interfaces/impl/TennisCoach_ScopeSingleton.java
@@ -3,6 +3,9 @@
  */
 package spring.configuration.annotations.scopes.interfaces.impl;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -37,5 +40,18 @@ public class TennisCoach_ScopeSingleton implements Coach {
     public String getDailyFortune() {
 	return fortuneService.getFortune();
     }
-
+    
+    // Define init method
+    // Rappel : Code will execute after constructor and after injection of dependencies
+    @PostConstruct
+    public void doInitStuffs() {
+	System.out.println(" >> TennisCoach_ScopeSingleton : inside doInitStuffs() method [@PostConstruct]");
+    }
+    
+    // Define destroy method
+    // Rappel : Code will execute before the bean is destroyed
+    @PreDestroy
+    public void doCleanupStuffs() {
+	System.out.println(" >> TennisCoach_ScopeSingleton : inside doCleanupStuffs() method [@PreDestroy]");
+    }
 }
